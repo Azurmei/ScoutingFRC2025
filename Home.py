e5e60476c2f5e3d06ed631284029395db7929b40
ShaneMatsushima <shanematsushima@gmail.com>
2025-01-10 11:42:21 -1000

    canpac update

--- Home.py
+++ Home.py
@@ -8,10 +8,10 @@ def main():
     st.write(f"V{version}")
     st.warning("This application is specific for Team 2438.")
     st.warning("Internet connection is required.")
+    st.success("Analytics and Form implemented for CanPac Regional")
     st.divider()
     st.write("This app is designed to help teams collect and analyze data from the 2025 FRC season.")
     st.write("Scouting reports can be added by selecting the tournament being scouted on the side bar.")
-    st.write("Analysis of reports will be created in a future update.")
     st.divider()
     st.write("For any issues or suggestions, please contact Shane. Thanks!")
 
