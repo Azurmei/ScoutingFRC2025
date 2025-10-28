3c60cda5a5da125c2b523dbed8e304739a6fa0ac
Lily Jin <150985200+Azurmei@users.noreply.github.com>
2025-10-27 16:27:26 -1000

    secrets.toml updates

--- Home.py
+++ Home.py
@@ -3,13 +3,14 @@ version = "0.3.22.25.1"
 
 
 def main():
-    st.title("Team2438 2025 Scouting App")
+    st.title("Team 2438 2026 Scouting Pass")
     st.write("Created by Shane Matsushima")
+    st.write("modified by Lily Jin")
     st.write(f"V{version}")
     st.warning("This application is specific for Team 2438.")
     st.warning("Internet connection is required.")
-    st.success("Analytics and Form implemented for CanPac Regional")
-    st.success("Analytics and Form implemented for Hawaii Regional")
+    #st.success("Analytics and Form implemented for CanPac Regional")
+    #st.success("Analytics and Form implemented for Hawaii Regional")
     st.divider()
     st.write("This app is designed to help teams collect and analyze data from the 2025 FRC season.")
     st.write("Scouting reports can be added by selecting the tournament being scouted on the side bar.")
