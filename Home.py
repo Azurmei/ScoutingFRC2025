ccbd5e54a10b417b567726cdeef20ac14b38cbdd
Lily Jin <150985200+Azurmei@users.noreply.github.com>
2025-11-12 16:30:38 -1000

    home updated

--- Home.py
+++ Home.py
@@ -1,19 +1,19 @@
 import streamlit as st
-version = "0.3.22.25.1"
+version = "2.0"
 
 
 def main():
-    st.title("Team 2438 2026 Scouting Pass")
+    st.title("2026 Scouting Pass ᯓ★")
     st.write("Created by Shane Matsushima")
-    st.write("modified by Lily Jin")
+    st.write("modified by Lily Jin ｡𖦹°‧")
     st.write(f"V{version}")
+    st.divider()
     st.warning("This application is specific for Team 2438.")
     st.warning("Internet connection is required.")
     #st.success("Analytics and Form implemented for CanPac Regional")
     #st.success("Analytics and Form implemented for Hawaii Regional")
     st.divider()
     st.write("This app is designed to help teams collect and analyze data from the 2025 FRC season.")
-    st.write("Scouting reports can be added by selecting the tournament being scouted on the side bar.")
     st.divider()
     st.write("For any issues or suggestions, please contact Shane. Thanks!")
 
