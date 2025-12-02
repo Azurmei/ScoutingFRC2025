07b4025f61fabddc96427ea03be6d9a7985dbdc9
Lily Jin <150985200+Azurmei@users.noreply.github.com>
2025-12-01 16:03:28 -1000

    api year 25 - >24

--- frc_api/frcApi.py
+++ frc_api/frcApi.py
@@ -1,47 +1,35 @@
+import base64
 import requests
 import streamlit as st
 
-# == Base Defines ==
-# API_KEY = st.secrets["frc_api"]
-API_KEY = st.secrets["frcapi"]["api_key"]
-BASE_URL = 'https://frc-api.firstinspires.org/v3.0'
-SEASON = 2025
+BASE_URL = "https://frc-api.firstinspires.org/v3.0"
+SEASON = 2024
 
+USERNAME = st.secrets["frcapi"]["username"]
+AUTH = st.secrets["frcapi"]["auth"]
 
-# Set up the headers with the Authorization token
+# Combine and encode
+token_raw = f"{USERNAME}:{AUTH}"
+token_bytes = token_raw.encode("ascii")
+auth_token = base64.b64encode(token_bytes).decode("ascii")
+
 headers = {
-    'Authorization': f'Basic {API_KEY}',
-    'Accept': 'application/json'  # Optional, depending on the API
+    "Authorization": f"Basic {auth_token}",
+    "Accept": "application/json"
 }
 
-def get_comp_teams(eventCode:str) -> list:
 
-    team_list = []    
-
-    # Get the competition teams
-    url = f'{BASE_URL}/{SEASON}/teams?eventCode={eventCode}'
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        raw_data = response.json()
-
-        for teams in raw_data['teams']:
-            team_list.append(teams['teamNumber'])
-        
-        return team_list
-    
-def get_comp_ranking(eventCode:str) -> list:
-
+def get_comp_teams(eventCode: str) -> list:
     team_list = []
+    url = f"{BASE_URL}/{SEASON}/teams?eventCode={eventCode}"
 
-    # Get the competition ranking
-    url = f'{BASE_URL}/{SEASON}/rankings/{eventCode}'
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        raw_data = response.json()
-        rank_data = raw_data["Rankings"]
-    
-        for rank in rank_data:
-            team_data = {"team#":rank["teamNumber"], "wins":rank["wins"], "losses":rank["losses"], "ties":rank["ties"], "avg_score":rank["qualAverage"], "dq":rank["dq"], "matches":rank["matchesPlayed"]}
-            team_list.append(team_data)
-    
+        data = response.json()
+        for t in data["teams"]:
+            team_list.append(t["teamNumber"])
+    else:
+        st.error(f"API Error: {response.status_code} — {response.text}")
+
     return team_list
