2e081db07e84cf10a7f3ba28668cf44720d17230
Lily Jin <150985200+Azurmei@users.noreply.github.com>
2026-03-01 03:04:38 +0000

    another commit

--- gs_client/gsClient.py
+++ gs_client/gsClient.py
@@ -1,30 +1,29 @@
 import gspread
-import json
 import streamlit as st
 from google.oauth2.service_account import Credentials
 
-scopes = [
-    'https://www.googleapis.com/auth/spreadsheets'
-]
+# === Google Sheets Setup ===
+scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
-credential_info = json.loads(st.secrets["google_sheets"]["credentials"])
-creds = Credentials.from_service_account_info(credential_info, scopes=scopes)
+# Load entire google_sheets block directly
+creds_info = dict(st.secrets["google_sheets"])
+
+creds = Credentials.from_service_account_info(creds_info, scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet_id = "1Sp-rPlrjyfu0UizkBmZKc__S3Sgy0g0Prl8s1wjFWHk"
 sheet = client.open_by_key(sheet_id)
 
-def append_data(worksheet, data:list) -> bool:
+# === Sheet Helper Functions ===
+def append_data(worksheet, data: list) -> bool:
     worksheet.append_row(data)
     return True
 
 def grab_all_data(worksheet):
-    data = worksheet.get_all_values()
-    return data
+    return worksheet.get_all_values()
 
-def check_duplicate(worksheet, data:list) -> bool:
+def check_duplicate(worksheet, data: list) -> bool:
     exist_data = worksheet.get_all_values()
-    print(len(exist_data))
-    if len(exist_data) == 1:
+    if len(exist_data) <= 1:
         return False
     return data in exist_data
